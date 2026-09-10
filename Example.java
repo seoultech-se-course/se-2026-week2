603565087f46ae6c65f4e1703c21a2415543f4cc
Min Seob Lee <107973815+MinCOnMinCon@users.noreply.github.com>
2026-09-10 17:28:39 +0900

    bfs 구현 (#2)

--- Example.java
+++ Example.java
@@ -1,13 +1,17 @@
+import java.util.Queue;
+import java.util.ArrayDeque;
+
 public class Example {
 
     public static void main(String[] args) {
         Node root = new Node("Root");
         Node child1 = new Node("Child 1");
         Node child2 = new Node("Child 2");
+        
 
         root.addChild(child1);
         root.addChild(child2);
-
+        
         System.out.println("Root label: " + root.label);
         System.out.println("Children of Root:");
         for (Node child : root.children) {
@@ -27,7 +31,23 @@ public class Example {
 
     }
     
-    private void bfs(){
-        
+    private static Node bfs(Node root, String key){
+        Queue<Node> non_visit = new ArrayDeque<>();
+        non_visit.offer(root);
+
+        while(!non_visit.isEmpty()){
+            Node visit = non_visit.poll();
+            if(visit.label.equals(key)){
+                return visit;
+            }
+            else{
+                for(int i = 0; i < visit.children.size(); i++){
+                    non_visit.offer(visit.children.get(i));
+                }
+            }
+        }
+
+        return null;
     }
+
 }
