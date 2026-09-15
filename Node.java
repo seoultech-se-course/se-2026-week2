78f2891162168fb93c775701c25e06cb3f133ceb
차재우 <motjjin123@gmail.com>
2026-09-15 22:09:27 +0900

    feat: dfs 메서드 구현

--- Node.java
+++ Node.java
@@ -14,4 +14,15 @@ public class Node {
     public void addChild(Node child) {
         this.children.add(child);
     }
+
+    // DFS (깊이 우선 탐색) 메서드 추가
+    public void dfs() {
+        // 1. 현재 노드의 라벨(이름)을 출력합니다.
+        System.out.print(this.label + " ");
+
+        // 2. 자식 노드들을 순서대로 방문하며 재귀적으로 dfs()를 호출합니다.
+        for (Node child : this.children) {
+            child.dfs();
+        }
+    }
 }
